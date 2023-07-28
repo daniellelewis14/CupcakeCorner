2c4f8e5e55edf91234c8de3b2a04de14554e652d
daniellelewis14 <yurts.third_0v@icloud.com>
2023-07-28 13:24:08 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Danielle Lewis on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
