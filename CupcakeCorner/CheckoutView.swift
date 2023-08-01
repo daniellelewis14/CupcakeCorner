00c70be000aee663da2e4b76ad82ad315637828d
daniellelewis14 <yurts.third_0v@icloud.com>
2023-08-01 14:07:27 -0400

    Create CheckoutView

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Danielle Lewis on 8/1/23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
