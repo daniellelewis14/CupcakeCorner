810a3013f867ecbd5e62dc2696e4f2e58fdfc67a
daniellelewis14 <yurts.third_0v@icloud.com>
2023-08-01 14:10:01 -0400

    Add Order object to CheckoutView

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CheckoutView()
+    CheckoutView(order: Order())
 }
